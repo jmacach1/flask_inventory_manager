1828e3c881b2c4ae8e25951a4d3fc5b57919e760
Macachor, Jerald Louie (Contractor) <JeraldLouie_Macachor@comcast.com>
2021-06-01 08:46:44 -0700

    product table view

--- mk_products.py
+++ mk_products.py
@@ -15,8 +15,11 @@ def create_product(name, price, quantity, description, category, unique_tag):
   db.session.commit()
 
 if __name__ == "__main__":
-  #
-  create_product("Bananas", 10.00, 200, "Yummy Bananas from Mexico", "Produce", "AAA111")
+  db.create_all()
+  create_product("Banana Leaf Table", 700.00, 200, "Table in shape of Banana Leaf", "Furniture", "FFF111")
+  create_product("Flower Chair", 200.00, 600, "Chairs shaped like flowers", "Furniture", "FFF222")
+  create_product("Islander Sandals", 20.00, 300, "Thick sole sandals", "Clothing", "CCC111")
+  create_product("Coconut Wine", 30.00, 50, "Wine from Coconut", "Beverage", "BBB111")
   # create_product("Oranges", 12.00, 50)
   products = Product.query.all()
   print(products)
